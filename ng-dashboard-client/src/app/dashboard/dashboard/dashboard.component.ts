10a343ee0f459e08985dbeaa65a7a208879f33de
Andrew <andrew@Shepherds-MacBook-Pro-2.local>
2021-04-03 15:37:43 +0800

    add row coloring based on value

--- ng-dashboard-client/src/app/dashboard/dashboard/dashboard.component.ts
+++ ng-dashboard-client/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GridApi } from 'ag-grid-community';
 import { UpdateData, WebsocketService } from '../../shared/services/websocket/websocket.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,28 +12,51 @@ export class DashboardComponent implements OnInit {
 
   columnDefs = [
     { headerName: 'Symbol', field: 'symbol' },
-    { headerName: 'Price', field: 'price' }, 
+    { headerName: 'Price', field: 'price' },
   ];
 
+  defaultColDef = {
+    flex: 1,
+    enableCellChangeFlash: true, 
+    resizable: true,
+  };
 
-  rowData = [];
+
+  rowData = Array<UpdateData>();
+
+  priceThreshhold = 1000;
+
+  rowClassRules = {
+    'row-red': params => params.data.price < 1000,
+    'row-green': params => params.data.price > 1000,
+  }
+
+  private gridApi: GridApi;
+  private gridColumnsApi;
+  private loaded: boolean;
 
   constructor(
     private websocketService: WebsocketService,
   ) { }
 
   ngOnInit(): void {
+  }
+
+  onGridReady(params: any): void {
+    this.gridApi = params.api;
+    this.gridColumnsApi = params.columnApi;
+    this.loaded = true;
     this.websocketService.getDataSubject().pipe(
-    ).subscribe(data =>{
-      this.handleIncomingData(data);
+      // takeUntil(componentDestroyed(this))x
+    ).subscribe(data => {
+        this.handleIncomingData(data);
     });
   }
 
   private handleIncomingData(data: Array<UpdateData>): void {
     this.rowData = [...data, ...this.rowData];
+    this.gridApi.redrawRows();
   }
-
-
 
 
 }
