73342cabbcdbcb8cf1f87faa3d9c055097fce5fe
Andrew <andrew@Shepherds-MacBook-Pro-2.local>
2021-04-03 16:47:36 +0800

    add db handler

---
+++ server/db_handler/db_handler_tests.py
@@ -0,0 +1,15 @@
+import unittest
+from db_handler import DbHandler
+
+class TestDbHandler(unittest.TestCase):
+
+    def test_save_to_db(self):
+        pass
+
+    def test_get_from_db(self):
+        pass
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
